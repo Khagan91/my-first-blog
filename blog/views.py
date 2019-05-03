ce36af30cdb1d967ba04c7cfc89553f7a631ce77
Kenneth <Kenneth>
2019-05-03 10:38:55 +0100

    updated code

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render, get_list_or_404
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
